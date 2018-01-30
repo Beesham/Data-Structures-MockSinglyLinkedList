401055b3144afec5023840c2bd5f0a7a05888aa6
Beesham <sarendranauth.beesham@gmail.com>
2018-01-29 22:24:11 -0500

    Add SinglyLinkedList skeleton

---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,44 @@
+public class SinglyLinkedList<E> implements LinkedListADT {
+
+	/*
+	Size of the linked list
+	 */
+	private int size = 0;
+
+	@Override
+	public void addFirst(Object o) {
+		//TODO
+	}
+
+	@Override
+	public void addLast(Object o) {
+		//TODO
+	}
+
+	@Override
+	public void removeFirst() {
+		//TODO
+	}
+
+	@Override
+	public void removeLast() {
+		//TODO
+	}
+
+	@Override
+	public Object getFirst() {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public Object getLast() {
+		//TODO
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+}
