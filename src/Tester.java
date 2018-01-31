3e65faff777fcbc37a40d56ac099781a9f0666c2
Beesham <sarendranauth.beesham@gmail.com>
2018-01-31 16:09:14 -0500

    Add Tester class

---
+++ src/Tester.java
@@ -0,0 +1,9 @@
+public class Tester {
+	public static void main(String[] args) {
+		SinglyLinkedList<String> list = new SinglyLinkedList<>();
+
+		list.addFirst("B");
+		list.addLast("e");
+		list.addLast("e");
+	}
+}
